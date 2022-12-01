c67df04710dbc74c91b6a2d5c880228b75c45696
Emirhan Sevinc <emirhansvncc@gmail.com>
2022-12-01 21:01:18 +0300

    Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 exports.createUser = async(req, res) => {
     try{
@@ -8,6 +9,29 @@ exports.createUser = async(req, res) => {
             status: 'Success',
             user,
         })
+    } catch (err) {
+        res.status(400).json({
+            status: 'Failed',  
+            err
+        })
+    }
+}
+
+exports. loginUser = async(req, res) => {
+    try{
+        const email = await req.body.email
+        const password = await req.body.password
+
+        User.findOne({email: email}, (err, user) => {
+            if(user){
+                bcrypt.compare( password, user.password, (err, same) => {
+                    if(same){
+                        res.status(200).send('You are logged in!')
+                    }
+                } )
+            }
+        })
+
     } catch (err) {
         res.status(400).json({
             status: 'Failed',  
