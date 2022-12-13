1446e32e2c55557841bc91fc86a3618ea82b04e1
Emirhan Sevinc <emirhansvncc@gmail.com>
2022-12-13 18:59:20 +0300

    Custom middlewares

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -3,9 +3,12 @@ const express = require('express')
 
 const router = express.Router()
 
+// Custom Middlewares
+const ifLoggedIn = require('../customMiddlewares//ifLoggedIn')
+
 router.route('/').get(pageController.getIndexPage)
 router.route('/about').get(pageController.getAboutPage)
-router.route('/register').get(pageController.getRegisterPage)
-router.route('/login').get(pageController.getLoginPage)
+router.route('/register').get(ifLoggedIn, pageController.getRegisterPage)
+router.route('/login').get(ifLoggedIn, pageController.getLoginPage)
 
 module.exports = router
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,14 @@
 const authController = require('../controllers/authController')
 const express = require('express')
 
+// Custom Middlewares
+const dashboardMiddleware = require('../customMiddlewares/dashboardMiddleware')
+
 const router = express.Router()
 
 router.route('/signup').post(authController.createUser)
 router.route('/login').post(authController.loginUser)
 router.route('/logout').get(authController.logout)
-router.route('/dashboard').get(authController.getDashboardPage)
+router.route('/dashboard').get(dashboardMiddleware, authController.getDashboardPage)
 
 module.exports = router
